908331f3b43f28f05a629d5ea8a5a47f3d212684
frankw <fwessels@xs4all.nl>
2017-10-25 16:16:35 -0700

    Initial AVX512 assembly for galMultiply and galMultiplyXor

--- galois_amd64.s
+++ galois_amd64.s
@@ -187,8 +187,85 @@ done_xor_sse2:
 
 // func galMulAVX512Xor(low, high, in, out []byte)
 TEXT ·galMulAVX512Xor(SB), 7, $0
+	MOVQ  low+0(FP), SI     // SI: &low
+	MOVQ  high+24(FP), DX   // DX: &high
+	MOVQ  $15, BX           // BX: low mask
+	MOVQ  BX, X5
+	MOVOU (SI), X6          // X6 low
+	MOVOU (DX), X7          // X7: high
+	MOVQ  in_len+56(FP), R9 // R9: len(in)
+
+	LONG $0x384de3c4; WORD $0x01f6             // VINSERTI128 YMM6, YMM6, XMM6, 1  ; low
+	LONG $0x3845e3c4; WORD $0x01ff             // VINSERTI128 YMM7, YMM7, XMM7, 1  ; high
+	LONG $0x48CDF362; WORD $0xF63A; BYTE $0x01 // VINSERTI64x4 ZMM6, ZMM6, YMM6, 1 ; low
+	LONG $0x48C5F362; WORD $0xFF3A; BYTE $0x01 // VINSERTI64x4 ZMM7, ZMM7, YMM7, 1 ; high
+	LONG $0x487D7262; WORD $0xC578             // VPBROADCASTB zmm8, xmm5          ; X8: lomask (unpacked)
+
+	SHRQ  $6, R9         // len(in) /64
+	MOVQ  out+72(FP), DX // DX: &out
+	MOVQ  in+48(FP), SI  // R11: &in
+	TESTQ R9, R9
+	JZ    done_xor_avx512
+
+loopback_xor_avx512:
+	LONG $0x48FEF162; WORD $0x066F             // VMOVDQU64 ZMM0, [rsi]
+	LONG $0x48FEF162; WORD $0x226F             // VMOVDQU64 ZMM4, [rdx]
+	LONG $0x48F5F162; WORD $0xD073; BYTE $0x04 // VPSRLQ   ZMM1, ZMM0, 4     ; Z1: high input
+	LONG $0x48FDD162; WORD $0xC0DB             // VPANDQ   ZMM0, ZMM0, ZMM8  ; Z0: low input
+	LONG $0x48F5D162; WORD $0xC8DB             // VPANDQ   ZMM1, ZMM1, ZMM8  ; Z1: high input
+	LONG $0x484DF262; WORD $0xD000             // VPSHUFB  ZMM2, ZMM6, ZMM0  ; Z2: mul low part
+	LONG $0x4845F262; WORD $0xD900             // VPSHUFB  ZMM3, ZMM7, ZMM1  ; Z3: mul high part
+	LONG $0x48EDF162; WORD $0xDBEF             // VPXORQ   ZMM3, ZMM2, ZMM3  ; Z3: Result
+	LONG $0x48E5F162; WORD $0xE4EF             // VPXORQ   ZMM4, ZMM3, ZMM4  ; Z4: Result
+	LONG $0x48FEF162; WORD $0x227F             // VMOVDQU64 [rdx], ZMM4
+
+	ADDQ $64, SI       // in+=64
+	ADDQ $64, DX       // out+=64
+	SUBQ $1, R9
+	JNZ  loopback_xor_avx512
+
+done_xor_avx512:
+	// VZEROUPPER
+	BYTE $0xc5; BYTE $0xf8; BYTE $0x77
 	RET
 
 // func galMulAVX512(low, high, in, out []byte)
 TEXT ·galMulAVX512(SB), 7, $0
+	MOVQ  low+0(FP), SI     // SI: &low table
+	MOVQ  high+24(FP), DX   // DX: &high table
+	MOVQ  $15, BX           // BX: low mask
+	MOVQ  BX, X5
+	MOVOU (SI), X6          // X6 low
+	MOVOU (DX), X7          // X7: high
+	MOVQ  in_len+56(FP), R9 // R9: len(in)
+
+	LONG $0x384de3c4; WORD $0x01f6             // VINSERTI128 YMM6, YMM6, XMM6, 1  ; low
+	LONG $0x3845e3c4; WORD $0x01ff             // VINSERTI128 YMM7, YMM7, XMM7, 1  ; high
+	LONG $0x48CDF362; WORD $0xF63A; BYTE $0x01 // VINSERTI64x4 ZMM6, ZMM6, YMM6, 1 ; low
+	LONG $0x48C5F362; WORD $0xFF3A; BYTE $0x01 // VINSERTI64x4 ZMM7, ZMM7, YMM7, 1 ; high
+	LONG $0x487D7262; WORD $0xC578             // VPBROADCASTB zmm8, xmm5          ; X8: lomask (unpacked)
+
+	SHRQ  $6, R9         // len(in) /64
+	MOVQ  out+72(FP), DX // DX: &out
+	MOVQ  in+48(FP), SI  // R11: &in
+	TESTQ R9, R9
+	JZ    done_avx512
+
+loopback_avx512:
+	LONG $0x48FEF162; WORD $0x066F             // VMOVDQU64 ZMM0, [rsi]
+	LONG $0x48F5F162; WORD $0xD073; BYTE $0x04 // VPSRLQ   ZMM1, ZMM0, 4     ; Z1: high input
+	LONG $0x48FDD162; WORD $0xC0DB             // VPANDQ   ZMM0, ZMM0, ZMM8  ; Z0: low input
+	LONG $0x48F5D162; WORD $0xC8DB             // VPANDQ   ZMM1, ZMM1, ZMM8  ; Z1: high input
+	LONG $0x484DF262; WORD $0xD000             // VPSHUFB  ZMM2, ZMM6, ZMM0  ; Z2: mul low part
+	LONG $0x4845F262; WORD $0xD900             // VPSHUFB  ZMM3, ZMM7, ZMM1  ; Z3: mul high part
+	LONG $0x48EDF162; WORD $0xE3EF             // VPXORQ   ZMM4, ZMM2, ZMM3  ; Z4: Result
+	LONG $0x48FEF162; WORD $0x227F             // VMOVDQU64 [rdx], ZMM4
+
+	ADDQ $64, SI       // in+=64
+	ADDQ $64, DX       // out+=64
+	SUBQ $1, R9
+	JNZ  loopback_avx512
+
+done_avx512:
+	BYTE $0xc5; BYTE $0xf8; BYTE $0x77 // VZEROUPPER
 	RET
