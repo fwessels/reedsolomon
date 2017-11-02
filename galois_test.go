1392b679fa013f0d0ce0fa8eeacef6df68e737ba
frankw <fwessels@xs4all.nl>
2017-11-01 18:24:16 -0700

    Process four input rows and four output rows simultaneously for AVX512

--- galois_test.go
+++ galois_test.go
@@ -378,3 +378,27 @@ func benchmarkGaloisXorParallel44(b *testing.B, size int) {
 func BenchmarkGaloisXorParallel44_10M(b *testing.B) {
 	benchmarkGaloisXorParallel44(b, 10*1024*1024)
 }
+
+func benchmarkGaloisXor512Parallel44(b *testing.B, size int) {
+	in := make([]byte, size)
+	in2 := make([]byte, size)
+	in3 := make([]byte, size)
+	in4 := make([]byte, size)
+	out := make([]byte, size)
+	out2 := make([]byte, size)
+	out3 := make([]byte, size)
+	out4 := make([]byte, size)
+
+	opts := defaultOptions
+	opts.useSSSE3 = true
+
+	b.SetBytes(int64(size * 16))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		galMulSliceXor512Parallel44(177, 25, 87, 111, in[:], out[:], in2[:], out2[:], in3[:], out3[:], in4[:], out4[:], false, true)
+	}
+}
+
+func BenchmarkGaloisXor512Parallel44_10M(b *testing.B) {
+	benchmarkGaloisXor512Parallel44(b, 10*1024*1024)
+}
